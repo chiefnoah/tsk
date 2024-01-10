48954bd8b3fa07adbd4fd65cb78fad17bf289b16
Noah Pederson <noah@packetlost.dev>
2024-01-09 21:33:12 -0600

    Starts adding some infrastructure for command parsing

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,15 @@
 #![allow(dead_code)]
 use crate::types::{Tag, Task, TaskId, TaskStatus};
 
+use combine::error::{ParseError, StreamError};
+use combine::parser::char::letter;
+use combine::stream::StreamErrorFor;
+use combine::{
+    easy, many1,
+    parser::choice::{choice, optional},
+    EasyParser, Parser, Stream,
+};
+
 /// `Query` represents a segment of a query when entering "query mode".
 enum Query {
     Tag(bool, Tag),
@@ -35,3 +44,31 @@ enum QueryCommand {
 }
 
 enum DetailCommand {}
+
+enum HomeParseResult {
+    Partial,
+    Complete,
+}
+
+#[derive(Debug)]
+pub(crate) enum CommandParseError {
+    UnknownCommand(String),
+    InvalidArgument(Vec<String>),
+    Unknown,
+}
+
+parser! {
+    fn command[Input]()(Input) -> String
+        where [Input: Stream<Token = char>]
+    {
+        choice((
+            char('r').and_then(|r: String|)
+        ))
+    }
+}
+
+fn parse_home_commands(input: &str) -> Result<HomeParseResult, CommandParseError> {
+    command()
+        .easy_parse(input.to_ascii_lowercase())
+        .map_err(|err| Err(CommandParseError::Unknown))
+}
